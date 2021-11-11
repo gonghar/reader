03d09afa12c52ea12b46e67bd677683a044a6a35
gonghar <62142902+gonghar@users.noreply.github.com>
2021-11-11 20:16:59 +0800

    Add files via upload

---
+++ reader1/assets/js/index.js
@@ -0,0 +1,23 @@
+// var in_animate = false;
+// $(function () {
+//     $(".books>div").mouseover(function () {
+//         if(!in_animate){
+//             $(this).animate({'width':'40%'}, 500)
+//             in_animate = true;
+//             setTimeout(function () {
+//                 in_animate = false
+//             }, 500)
+//         }
+//
+//     });
+//     $(".books>div").mouseout(function () {
+//         if(!in_animate){
+//             $(this).animate({'width':'25%'}, 500)
+//             in_animate = true;
+//             setTimeout(function () {
+//                 in_animate = false
+//             }, 500)
+//         }
+//
+//     });
+// });
